36bc72bd5aa976ce6f20398442559d989eae87f2
silva4dev <web4dev.lucas@outlook.com>
2022-10-07 21:05:06 -0300

    inicial

---
+++ src/components/Form/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FaPlus } from 'react-icons/fa';
+import './Form.css';
+
+export default function Form({ handleChange, handleSubmit, novaTarefa }) {
+  return (
+    <form onSubmit={handleSubmit} action="#" className="form">
+      <input value={novaTarefa} onChange={handleChange} type="text" />
+      <button type="submit">
+        <FaPlus />
+      </button>
+    </form>
+  );
+}
+
+Form.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+  novaTarefa: PropTypes.string.isRequired,
+};
---
+++ src/components/Tarefas/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FaEdit, FaWindowClose } from 'react-icons/fa';
+import './Tarefas.css';
+
+export default function Tarefas({ handleEdit, handleDelete, tarefas }) {
+  return (
+    <ul className="tarefas">
+      {tarefas.sort().map((tarefa, index) => (
+        <li key={tarefa}>
+          {tarefa}
+          <span>
+            <FaEdit onClick={(e) => handleEdit(e, index)} className="edit" />
+            <FaWindowClose
+              onClick={(e) => handleDelete(e, index)}
+              className="delete"
+            />
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+Tarefas.propTypes = {
+  handleEdit: PropTypes.func.isRequired,
+  handleDelete: PropTypes.func.isRequired,
+  tarefas: PropTypes.array.isRequired,
+};
